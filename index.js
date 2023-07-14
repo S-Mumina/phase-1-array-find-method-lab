efc91490642dca51a6b78213c5bb5137282e894d
muasya <muasyamumina@gmail.com>
2023-07-14 13:21:35 +0300

    completed assignment for array.find method

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// code your solution here.
+function superbowlWin(record) {
+  const broncosWin = record.find(game => game.result === "W");
+  return broncosWin ? broncosWin.year : undefined;
+}
+
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+ 
+];
+
+console.log(superbowlWin(record));
+
+
+
+
+
+
+
+
+
+
+
+
